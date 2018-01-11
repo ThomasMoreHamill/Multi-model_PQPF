8ab4b31d828c7b3b37a88a4da7b3072298c07e0b
ThomasMoreHamill <tom.hamill@noaa.gov>
2018-01-11 13:47:43 -0700

    Add files via upload
    updated to use different forecast output file names.

--- display_prob_forecasts_mme/display_prob_forecasts_mme.py
+++ display_prob_forecasts_mme/display_prob_forecasts_mme.py
@@ -17,6 +17,41 @@ from numpy import ma
 import os, sys
 from dateutils import daterange, dateshift, dayofyear, splitdate
 
+def read_forecast_probabilities (data_directory, center, cleade, \
+    cyyyymmddhh, cempirical, cgammadress):
+    
+    if cempirical == '1' and cgammadress == '1':
+        infile = data_directory+center+'/'+center+'_'+cleade+'h_IC'+cyyyymmddhh+'_empirical_gammadress.nc'
+    elif cempirical == '1' and cgammadress == '0':
+        infile = data_directory+center+'/'+center+'_'+cleade+'h_IC'+cyyyymmddhh+'_empirical_gaussdress.nc'
+    elif cempirical == '0' and cgammadress == '0':
+        infile = data_directory+center+'/'+center+'_'+cleade+'h_IC'+cyyyymmddhh+'_gammaqmap_gaussdress.nc'
+    elif cempirical == '0' and cgammadress == '1':
+        infile = data_directory+center+'/'+center+'_'+cleade+'h_IC'+cyyyymmddhh+'_gammaqmap_gammadress.nc'
+    
+    fexist  = os.path.exists(infile)
+    if fexist:
+        try:
+            nc = Dataset(infile)
+            conusmask = nc.variables['conusmask'][:,:]
+            rlonsa = nc.variables['rlonsa'][:,:]
+            rlatsa = nc.variables['rlatsa'][:,:]
+            prob_forecast = nc.variables['prob_forecast'][ithresh,:,:]
+            nc.close()  
+        except (IOError, ValueError, RuntimeError):
+            print 'Error reading ', infile
+            print 'IOError = ', IOError
+            print 'ValueError = ',ValueError
+            print 'RuntimeError  = ',RuntimeError
+            print 'Quitting.'
+            print sys.exit()
+    else:
+        print 'File = ',infile,'does not exist.  Quitting'
+        sys.exit()
+    
+    return rlonsa, rlatsa, conusmask, prob_forecast
+
+
 # --- setting up font sizes for the display
 
 rcParams['legend.fontsize']='small'
@@ -30,7 +65,11 @@ rcParams['contour.negative_linestyle']='solid'
 cyyyymmddhh = sys.argv[1] # initial time in YYYYMMDDHH format
 cleade = sys.argv[2] # end lead time in hours
 cthresh = sys.argv[3] # threshold amount
+cempirical = sys.argv[4] # =1 for empirical CDF, 0 for Gamma 
+cgammadress = sys.argv[5] # = 1 for full gamma-dist dressing 0 for simple Gaussian
 ileade = int(cleade)
+cleadb = str(int(cleade)-12)
+date_verif = dateshift(cyyyymmddhh, ileade)
 
 if cthresh == 'POP':
     ithresh = 0
@@ -67,11 +106,18 @@ cmonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','De
 cmonth = cmonths[mm-1]
 iyyyymmddhh = int(cyyyymmddhh)
 
+yyyy_verif,mm_verif,dd_verif,hh_verif = splitdate(date_verif)
+cyyyy_verif = str(yyyy_verif)
+cdd_verif = str(dd_verif)
+chh_verif = str(hh_verif)
+cmonth_verif = cmonths[mm_verif-1]
+
+
 # ---- read in precipitation analysis
 
 
-#data_directory = '/Users/thamill/precip/ecmwf_data/'
-data_directory = '/Projects/Reforecast2/netcdf/NationalBlend/'
+data_directory = '/Users/thamill/precip/ecmwf_data/'
+#data_directory = '/Projects/Reforecast2/netcdf/NationalBlend/'
 filename= data_directory + 'precip_analyses_ccpa_v1_2002010100_to_2016123100.nc'
 print 'reading ',filename
 nc = Dataset(filename)
@@ -92,32 +138,23 @@ apcp_anal = nc.variables['apcp_anal'][idx_late[0],:,:] + \
 mninetynine = -99.99*np.ones((nya,nxa), dtype=np.float32)
 nc.close()
 
-# ---- read in the NCEP ensemble
+# ---- read in the NCEP ensemble probabilities
 
-infile = data_directory+'NCEP_'+cleade+'h_IC'+cyyyymmddhh+'.nc'
-print infile
-nc = Dataset(infile)
-conusmask = nc.variables['conusmask'][:,:]
-rlonsa = nc.variables['rlonsa'][:,:]
-rlatsa = nc.variables['rlatsa'][:,:]
-prob_forecast_NCEP = nc.variables['prob_forecast'][ithresh,:,:]
-nc.close()
+rlonsa, rlatsa, conusmask, prob_forecast_NCEP = \
+    read_forecast_probabilities (data_directory, 'NCEP', cleade, \
+    cyyyymmddhh, cempirical, cgammadress)
 
-# ---- read in the CMC ensemble
+# ---- read in the CMC ensemble probabilities
 
-infile = data_directory+'CMC_'+cleade+'h_IC'+cyyyymmddhh+'.nc'
-print infile
-nc = Dataset(infile)
-prob_forecast_CMC = nc.variables['prob_forecast'][ithresh,:,:]
-nc.close()
+rlonsa, rlatsa, conusmask, prob_forecast_CMC = \
+    read_forecast_probabilities (data_directory, 'CMC', cleade, \
+    cyyyymmddhh, cempirical, cgammadress)
 
-# ---- read in the ECMWF ensemble
+# ---- read in the ECMWF ensemble probabilities
 
-infile = data_directory+'ECMWF_'+cleade+'h_IC'+cyyyymmddhh+'.nc'
-print infile
-nc = Dataset(infile)
-prob_forecast_ECMWF = nc.variables['prob_forecast'][ithresh,:,:]
-nc.close()
+rlonsa, rlatsa, conusmask, prob_forecast_ECMWF = \
+    read_forecast_probabilities (data_directory, 'ECMWF', cleade, \
+    cyyyymmddhh, cempirical, cgammadress)
 
 # ---- create the final output forecast as a weighted linear combination
 #      of the three inputs.
@@ -130,8 +167,21 @@ prob_forecast_MME = 0.5*prob_forecast_ECMWF + 0.25*prob_forecast_CMC + \
 # ---- plot a four-panel figure with raw, qmapped, final, verif
 
 fig1 = plt.figure(figsize=(7.8,6.5))
+  
+if cempirical == 1:
+    cemptitle = ' empirical CDFs'
+else:
+    cemptitle = ' Gamma CDFs'
+    
+if cgammadress == 1:
+    cgammatitle = ' Gamma-distribution dressing'
+else:
+    cgammatitle = ' simplified Gaussian dressing'
+    
 plt.suptitle(r''+cleade+'-h statistically post-processed forecast of '+cthresh_title+\
-    '\ninitialized 00 UTC '+cdd+' '+cmonth+' '+cyyyy,fontsize=16)
+    ' initialized\n00 UTC '+cdd+' '+cmonth+' '+cyyyy+', '+cemptitle+\
+    ' and'+cgammatitle,fontsize=14)
+    
 
 for ifield in range(4):
     if ifield == 0:
@@ -188,6 +238,78 @@ for ifield in range(4):
 plot_title = 'MME_'+cthresh+'_'+cyyyymmddhh+'_'+cleade+'h.pdf'
 fig1.savefig(plot_title)
 print 'saving plot to file = ',plot_title
+
+
+
+# ======================================================================
+
+# ---- plot a two-panel figure with MME + verif
+
+fig1 = plt.figure(figsize=(7.8,3.7))
+
+plt.suptitle(r'' + cleade + '-h MME forecast of ' + cthresh_title + ' and verification, '\
+    +' with forecast initialized\n00 UTC ' + cdd + ' ' + cmonth + ' ' + cyyyy +\
+    cemptitle + ' and' + cgammatitle,fontsize=14)
+
+for ifield in range(4):
+    if ifield == 0:
+        prob_forecast_display = prob_forecast_MME # climo_prob[ithresh,:,:] #
+        position = [0.02, 0.1, 0.46, 0.68]
+        position_legend = [0.02, 0.095, 0.46, 0.04]
+        ctitle = '(a) Post-processed multi-model probability' # Climatological probability' #
+        colorst = ['White','#E4FFFF','#C4E8FF','#8FB3FF','#D8F9D8','#A6ECA6','#42F742','Yellow','Gold',\
+            'Orange','#FCD5D9','#F6A3AE','#FA5257','Red','Maroon','#A449FF','LightGray'] #'#AD8ADB
+        colorstblack=['White','Black','White','White', 'White','White',\
+            'White','White','White', 'White','White','White','White','White','White']
+        levs = [0.0, 0.03, 0.05,0.1, 0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99,1.0]
+        linewidths = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        legend_title = 'Probability'
+        
+    elif ifield == 1:
+        prob_forecast_display = apcp_anal
+        position = [0.52, 0.1, 0.46, 0.68]
+        position_legend = [0.52, 0.095, 0.46, 0.04]
+        ctitle = '(b) CCPA 12-h accumulated precipitation analysis\n'+\
+            'valid '+chh_verif+' UTC '+cdd_verif+' '+cmonth_verif+' '+cyyyy_verif
+        colorst = ['White','#E4FFFF','#C4E8FF','#8FB3FF','#D8F9D8','#A6ECA6','#42F742','Yellow','Gold',\
+            'Orange','#FCD5D9','#F6A3AE','#FB4246','Red','#AD8ADB','#A449FF','LightGray'] #'#AD8ADB
+        colorstblack=['White','Black','Black','Black','Black', 'Black','Black','Black',\
+            'Black','Black','Black','Black','Black','Black','Black','Black','Black']
+        colorstwhite=['White','Black','Black','White','White','White','White',\
+            'White','White','White','Black','White','White','White','White','White']
+        clevs = [0.0, 0.254,1,2,3,4,5,7,10,15,20,30,50,100]
+        linewidths = [0.0,0.4,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        legend_title = 'Precipitation amount (mm)'
+
+    ax = fig1.add_axes(position)
+    ax.set_title(ctitle,fontsize=10.5)
+    m = Basemap(projection='mill',llcrnrlon=rlonsa[0,0],llcrnrlat=rlatsa[0,0],\
+        urcrnrlon=rlonsa[-1,-1],urcrnrlat=rlatsa[-1,-1],resolution='l')
+    x,y = m(rlonsa,rlatsa)
+    prob_forecast_m = ma.array(prob_forecast_display)
+    if ifield == 1: \
+        CS1 = m.contour(x,y,prob_forecast_m,clevs,\
+            colors=colorstblack,cmap=None,linewidths = linewidths)
+    CS2 = m.contourf(x,y,prob_forecast_m,clevs,colors=colorst,cmap=None,extend='neither')
+    m.drawcoastlines(linewidth=.5)
+    m.drawstates(linewidth=.5)
+    m.drawcountries(linewidth=.5)
+
+    cax = fig1.add_axes(position_legend)
+    cbar = fig1.colorbar(CS2,extend='neither', \
+        orientation='horizontal',cax=cax,drawedges=True,ticks=clevs,format='%g')
+    cax.set_xlabel(legend_title,fontsize=9)
+    cbar.ax.tick_params(labelsize=6)
+
+# ---- set plot title, save to pdf file
+
+plot_title = 'MME_2panel_'+cthresh+'_'+cyyyymmddhh+'_'+cleade+'h.pdf'
+fig1.savefig(plot_title)
+print 'saving plot to file = ',plot_title
+
+
+
+
 
 
 
